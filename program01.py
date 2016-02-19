94257fa12fe89d0dc521ffd9a86130cb80fe5844
csc201spring2016 <cprudence@uri.edu>
2016-02-19 12:09:03 -0500

    added half pyramid

---
+++ program01.py
@@ -0,0 +1,9 @@
+#Half Pyramid
+#user input
+user_input = input("Enter a word")
+user_input = user_input.lower()
+
+#while loop to check if the user entered the word stop
+while(user_input != "stop"):
+    user_input = input("Enter a word")
+    user_input = user_input.lower()
